677a4a78d2577d8d50e19eb996d238fc01c8ef95
Ayodeji Okusanya <ayodeji.okusanya@gmail.com>
2018-10-30 20:18:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ document.querySelector("main#main").remove();
 
 /* Create your new element here and assign it to newHeader */
 const newHeader = document.createElement("h1");
-newHeader.id = "victory"
 newHeader.innerText = "Steven is the champion!";
+newHeader.id = "victory"
+
 document.body.appendChild(newHeader)
