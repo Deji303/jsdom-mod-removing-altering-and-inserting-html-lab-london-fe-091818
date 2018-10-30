bb5bf97fdc5e5f962848321a7ec7db225baabbdf
Ayodeji Okusanya <ayodeji.okusanya@gmail.com>
2018-10-30 20:05:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 /* Enter the code to remove the main node element under this comment */
+var dog = document.body.querySelector("main");
+dog.remove();
+
 
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+// const newHeader = null;
+
+let newHeader = document.createElement('h1');
+newHeader.innerHTML = "YOUR-NAME is the champion";
+newHeader.id = "victory";
+document.body.appendChild(newHeader);
+
